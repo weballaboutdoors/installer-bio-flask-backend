7e71974bc82cd4227a2622092a5297b32970d84f
Brett Bates <brett.aadw@gmail.com>
2024-09-06 08:55:20 -0500

    preparing to commit for heroku

--- app.py
+++ app.py
@@ -386,6 +386,7 @@ app, socketio, limiter, connection_pool = create_app()
 
 if app is not None:
     if __name__ == '__main__':
-        app.run(debug=True, host='0.0.0.0', port=5001)
+        port = int(os.environ.get('PORT', 5000))
+        app.run(host='0.0.0.0', port=port)
 else:
     print("Failed to create app")
