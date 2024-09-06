65ab3f99f8dcdd0cad16f6c3bb79e69d2a0a7e53
Brett Bates <brett.aadw@gmail.com>
2024-09-06 11:06:14 -0500

    Add database backup script

--- backup_jawsdb.sh
+++ backup_jawsdb.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-# Get the JAWSDB_URL
-JAWSDB_URL=$(heroku config:get JAWSDB_URL)
+# The JAWSDB_URL is already set as an environment variable in Heroku
+# so we don't need to use the heroku command to get it
 
 # Parse the JAWSDB_URL
 DB_USER=$(echo $JAWSDB_URL | cut -d: -f2 | cut -d/ -f3)
@@ -10,14 +10,40 @@ DB_HOST=$(echo $JAWSDB_URL | cut -d@ -f2 | cut -d: -f1)
 DB_PORT=$(echo $JAWSDB_URL | cut -d: -f4 | cut -d/ -f1)
 DB_NAME=$(echo $JAWSDB_URL | cut -d/ -f4)
 
+# Print parsed values for debugging (remove in production)
+echo "Host: $DB_HOST"
+echo "Port: $DB_PORT"
+echo "User: $DB_USER"
+echo "Password: $DB_PASSWORD"
+echo "Database: $DB_NAME"
+
 # Perform the backup
 BACKUP_FILE="backup_$(date +%Y%m%d_%H%M%S).sql"
-mysqldump --host=$DB_HOST --port=$DB_PORT --user=$DB_USER --password=$DB_PASSWORD $DB_NAME > $BACKUP_FILE
+
+# Use mysql instead of mysqldump
+mysql --host=$DB_HOST --port=$DB_PORT --user=$DB_USER --password=$DB_PASSWORD $DB_NAME -e "
+SET group_concat_max_len = 1024 * 1024 * 1024;
+SELECT CONCAT(
+    'DROP TABLE IF EXISTS \`', table_name, '\`;',
+    '\n',
+    'CREATE TABLE \`', table_name, '\` (',
+    GROUP_CONCAT(CONCAT(
+        '\n  \`', column_name, '\` ', column_type,
+        IF(is_nullable = 'NO', ' NOT NULL', ''),
+        IF(column_default IS NOT NULL, CONCAT(' DEFAULT \'', column_default, '\''), ''),
+        IF(extra != '', CONCAT(' ', extra), '')
+    ) ORDER BY ordinal_position SEPARATOR ','),
+    '\n);'
+) AS create_table
+FROM information_schema.columns
+WHERE table_schema = DATABASE()
+GROUP BY table_name;" > $BACKUP_FILE
+
+for table in $(mysql --host=$DB_HOST --port=$DB_PORT --user=$DB_USER --password=$DB_PASSWORD $DB_NAME -e "SHOW TABLES;" | tail -n +2); do
+    echo "Backing up table: $table"
+    mysql --host=$DB_HOST --port=$DB_PORT --user=$DB_USER --password=$DB_PASSWORD $DB_NAME -e "SELECT * FROM \`$table\`;" >> $BACKUP_FILE
+done
 
 echo "Backup completed: $BACKUP_FILE"
-
-# Optional: Upload the backup file to a storage service (e.g., AWS S3)
-# aws s3 cp $BACKUP_FILE s3://your-bucket-name/
-
-# Clean up
-rm $BACKUP_FILE
+echo "Backup contents:"
+cat $BACKUP_FILE
