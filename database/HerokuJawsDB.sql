a92a22a1cc49859174d9eb49e87681390c272b90
Brett Bates <brett.aadw@gmail.com>
2024-09-10 10:59:39 -0500

    add databse to directory

---
+++ database/HerokuJawsDB.sql
@@ -0,0 +1,14 @@
+USE k5whq2461jtx4q01;
+INSERT INTO installer_skills (installer_id, skillset) VALUES
+(26, 'Window Installation'),
+(20, 'Door Installation'),
+(23, 'Wood Rot Repair'),
+(23, 'Window Replacement'),
+(23, 'Siding Installation');
+
+INSERT INTO installer_jobs (installer_id, job_type) VALUES
+(26, 'New Construction'),
+(24, 'Renovation'),
+(24, 'Repair'),
+(24, 'Replacement'),
+(23, 'Exterior Work');
